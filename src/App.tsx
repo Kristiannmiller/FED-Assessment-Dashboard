c8dd74b26dae5d67f3a460cfdb284e34ade0f09e
Sakina Shivji <sshivji@rightpoint.com>
2023-02-21 15:14:49 -0600

    refactor - remove unused imports from App and replace function with const

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import ScoreForm from "./components/ScoreForm/ScoreForm";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
     <div className="App">
       <h1>FED Assessment User View Dashboard</h1>
       <ScoreForm />
     </div>
   );
-}
+};
 
 export default App;
