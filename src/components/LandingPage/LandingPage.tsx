11f51b75c8231ad32c36e108c2f801e0fc631179
Sakina Shivji <sshivji@rightpoint.com>
2023-02-23 09:04:33 -0600

    build - initial styles for Landing Page

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,15 +1,28 @@
 import "./LandingPage.scss";
+import logo from "../../assets/images/RP-logo.png";
 
 const LandingPage = () => {
   return (
     <div className="landingPage">
+      <div className="logo">
+        <img src={logo} />
+      </div>
       <h1>Welcome to the FED Assessment Dashboard</h1>
       <h3>Let's get you logged in</h3>
       <div className="email">
-        <label htmlFor="email">Email Address</label>
+        <label htmlFor="email" className="email--label">
+          Email Address
+        </label>
 
-        <input type="email" id="email" required></input>
-        <button type="submit">Login</button>
+        <input
+          type="email"
+          id="email"
+          required
+          className="email--input"
+        ></input>
+        <button type="submit" className="email--btn">
+          Login
+        </button>
       </div>
     </div>
   );
