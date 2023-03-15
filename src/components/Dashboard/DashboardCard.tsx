21119fc8a6f79b067d75bd3398d7798eaef8cae7
kristiannmiller <kristi.ann.miller@gmail.com>
2023-03-15 16:43:39 -0600

    chore/add-card-flip-animation

--- src/components/Dashboard/DashboardCard.tsx
+++ src/components/Dashboard/DashboardCard.tsx
@@ -8,14 +8,69 @@ const DashboardCard: React.FC<CardCategory> = ({
 }) => {
   return (
     <div className='cardWrap'>
-      <div className='cardHeader'>
-        <h2>{category}</h2>
-      </div>
-      <div className='cardBody'>
-        <h3>overall score</h3>
-        <p>
-          <span className='score'>{score}</span>/{totalScore}
-        </p>
+      <div className='cardInner'>
+        <div className='cardFront'>
+          <div className='cardHeader'>
+            <h2>{category}</h2>
+          </div>
+          <div className='cardBody'>
+            <h3>overall score</h3>
+            <p>
+              <span className='score'>{score}</span>/{totalScore}
+            </p>
+          </div>
+        </div>
+        <div className='cardBack'>
+          <div className='cardHeader'>
+            <h2>{category}</h2>
+          </div>
+          <div className='cardBody'>
+            <ul>
+              <li>
+                <span>category 1</span>
+                <span>10/10</span>
+              </li>
+              <li>
+                <span>category 2</span>
+                <span>9/10</span>
+              </li>
+              <li>
+                <span>category 3</span>
+                <span>5/10</span>
+              </li>
+              <li>
+                <span>category 4</span>
+                <span>10/10</span>
+              </li>
+              <li>
+                <span>category 5</span>
+                <span>6/10</span>
+              </li>
+              <li>
+                <span>category 6</span>
+                <span>8/10</span>
+              </li>
+              <li>
+                <span>category 7</span>
+                <span>7/10</span>
+              </li>
+              <li>
+                <span>category 8</span>
+                <span>1/10</span>
+              </li>
+              <li>
+                <span>category 9</span>
+                <span>3/10</span>
+              </li>
+              <li>
+                <span>
+                  category 10 is super long and will take up two lines
+                </span>
+                <span>3/10</span>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
